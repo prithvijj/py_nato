63d7b96c61158ae1622a084973f79e791529268e
pbj <pbj@pop-os.localdomain>
2019-08-20 21:57:11 +0400

    Added code

---
+++ py_nato.py
@@ -0,0 +1,15 @@
+# File converts characters into NATO phonetic alphabets
+import sys
+
+dict_nato = {'A':'Alpha', 'B':'Bravo','C':'Charlie', 'D':'Delta', 'E':'Echo', 'F':'Foxtrot', 'G':'Golf',"H":"Hotel", 'I':'India', 'J':'Juliet', 'K':'Kilo', 'L':'Lima', 'M':'Mike', 'N':'November', 'O':'Oscar', 'P':'Papa', 'Q':'Quebec', 'R':'Romeo', 'S':'Sierra', 'T':'Tango', 'U':'Uniform', 'V':'Victor', 'W':'Whiskey', 'X':'Xray', 'Y':'Yankee', 'Z':'Zulu'}
+
+argument_number = len(sys.argv) - 1 
+
+if (argument_number == 1):
+	# input_string = raw_input('Enter a string:')
+	input_string = sys.argv[1]
+	print(" ".join(map(dict_nato.get,input_string.replace(" ","").upper())))
+else:
+	print("Enter a word as an argument.")
+
+
